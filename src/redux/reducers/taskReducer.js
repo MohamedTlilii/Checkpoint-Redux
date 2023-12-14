e97a235db5f5e5794e7cebed8ac2c5afc524aafa
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-14 14:23:19 +0100

    almost-done

---
+++ src/redux/reducers/taskReducer.js
@@ -0,0 +1,32 @@
+const initialState = {
+  tasks: [],
+  filter: "all", // possible values: "all", "done", "notDone"
+};
+
+const taskReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TASK":
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload],
+      };
+    case "TOGGLE_TASK":
+      return {
+        ...state,
+        tasks: state.tasks.map((task) =>
+          task.id === action.payload
+            ? { ...task, isDone: !task.isDone }
+            : task
+        ),
+      };
+    case "SET_FILTER":
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default taskReducer;
